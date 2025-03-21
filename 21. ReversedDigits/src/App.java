61c5b4cc6cffab8c5dd5ab1bd6c4b645749baa32
Camilo Ramirez <camiestebanramirezsalas@gmail.com>
2025-03-20 23:05:31 -0500

    Day 21, reversed numbers :)

--- 21. ReversedDigits/src/App.java
+++ 21. ReversedDigits/src/App.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Write an integer: ");
+        int number = sc.nextInt();
+        System.out.println("The number reversed is: " + reversed(number));
+        sc.close();
+    }
+
+    public static int reversed(int number){
+        String result = "";
+        if (number<0) {
+            result += "-";
+            number *= -1;
+        }
+        String numberStr = Integer.toString(number);
+        for (int i = numberStr.length()-1; i >= 0; i--) 
+            result += numberStr.charAt(i);
+        
+        return Integer.parseInt(result);
     }
 }
