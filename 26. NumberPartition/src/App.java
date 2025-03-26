e9d2b8ca00fe6f0286c3df55f2342c37e6705235
Camilo Ramirez <camiestebanramirezsalas@gmail.com>
2025-03-25 21:30:00 -0500

    Day 26, Partition numbers :)

--- 26. NumberPartition/src/App.java
+++ 26. NumberPartition/src/App.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Write the partition number you want: ");
+        int number = sc.nextInt();
+        System.out.println("The result is: " + partition(number));
+        sc.close();
     }
 
-    
+    public static int partition(int number){
+        int result = 0;
+        if (number==0) 
+            result = 1;
+        else{
+            for (int i = 1; i <= number; i++) {
+                int gk = (i*(3*i-1))/2;
+                result += Math.pow(-1, i+1) * (partition(number - gk) + partition(number- gk-i));
+            }
+        }
+        return result;
+    }
 }
