aed25b3cd79abd5bf5c9e31882972a2bb84dd4db
Camilo Ramirez <camiestebanramirezsalas@gmail.com>
2025-04-01 20:50:10 -0500

    .

--- 29-35. ContactManagerProject/src/App.java
+++ 29-35. ContactManagerProject/src/App.java
@@ -1,8 +1,10 @@
+import model.Contact;
 import persistance.FileManager;
 
 public class App {
     public static void main(String[] args) throws Exception {
         FileManager manager = new FileManager();
-        manager.initFile();
+        Contact contact = new Contact("Pepe", "3122395518", "pepe@gmail.com");
+        manager.addContact(contact);
     }
 }
--- 29-35. ContactManagerProject/src/persistance/FileManager.java
+++ 29-35. ContactManagerProject/src/persistance/FileManager.java
@@ -4,20 +4,54 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import model.Contact;
 
 public class FileManager {
+    private String filePath = "resources/contacts.json";
+
     public void initFile() throws IOException {
-        String filePath = "resources/contacts.json";
+        
         File file = new File(filePath);
 
-        if (!file.exists()) {
-            file.createNewFile();
-        }
+        if (!file.exists()) 
+            file.createNewFile();        
 
         if (file.length() == 0) {
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
                 writer.write("[]");
             }
         }
+    }
+
+    public boolean addContact(Contact contact) throws IOException {
+        initFile();
+
+        RandomAccessFile raf = new RandomAccessFile(filePath, "rw");
+        long fileLength = raf.length();
+
+        raf.seek(fileLength - 1);
+
+        if (fileLength > 2) { 
+            raf.writeBytes(",\n");
+        } else {
+            raf.seek(1);
+            raf.writeBytes("\n");
+        }
+
+        String contactJson = "    " + chainObjectJson(contact).replace("\n", "\n    ");
+        raf.writeBytes(contactJson + "]");
+
+        raf.close();
+        return true;
+    }
+
+    private String chainObjectJson(Contact contact) {
+        return "{" + "\n" +
+                "    \"name\": \"" + contact.getName() + "\"," + "\n" +
+                "    \"phone\": \"" + contact.getNumber() + "\"," + "\n" +
+                "    \"email\": \"" + contact.getEmail() + "\"" + "\n" +
+                "}";
     }
 }
